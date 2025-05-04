0dc3f2232f52aadf5efa36b95b8b63dca43f4e8b
Kuba <qubatron15@gmail.com>
2025-05-04 21:39:21 +0200

    initial commit - StickerSmash template

--- StickerSmash/app/index.tsx
+++ StickerSmash/app/index.tsx
@@ -1,6 +1,70 @@
-import { Text, View } from "react-native";
+import { Button, PermissionsAndroid, Platform, Text, View } from "react-native";
+import {
+  checkIfHasSMSPermission,
+  // requestReadSMSPermission,
+  startReadSMS,
+ } from '@maniac-tech/react-native-expo-read-sms';
+import { useState } from "react";
+
+export async function requestReadSMSPermission() {
+  console.log('ooooo');
+  if (Platform.OS === "android") {
+    debugger;
+    try {
+      const hasPermission = await checkIfHasSMSPermission();
+      if (hasPermission.hasReadSmsPermission && hasPermission.hasReceiveSmsPermission) {
+        return true;
+      }
+
+      const status = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.RECEIVE_SMS,
+        PermissionsAndroid.PERMISSIONS.READ_SMS,
+        // PermissionsAndroid.PERMISSIONS.READ_CONTACTS
+      ]);
+
+      console.log('+++++++++++++++', status);
+      // if (status === PermissionsAndroid.RESULTS.GRANTED) return true;
+      // if (status === PermissionsAndroid.RESULTS.DENIED) {
+      //   console.log("Read Sms permission denied by user.", status);
+      // } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+      //   console.log("Read Sms permission revoked by user.", status);
+      // }
+
+      return false;
+    } catch (error) {
+      console.error("Error requesting SMS permissions", error);
+      return false;
+    }
+  }
+  return true;
+}
 
 export default function Index() {
+  const [isListening, setIsListening] = useState(false);
+
+  const smsReceived = (data: any) => {
+    console.log(data);
+  }
+
+  const startReadingSMS = async () => {
+    console.log('requesting');
+    const hasPermission = await checkIfHasSMSPermission();
+
+    if(hasPermission.hasReadSmsPermission && hasPermission.hasReceiveSmsPermission) {
+      setIsListening(true)
+      startReadSMS(smsReceived);
+    } else {
+      setIsListening(false);
+      // console.log(requestReadSMSPermission);
+      debugger; // requesting
+      const result = await requestReadSMSPermission();
+      debugger; // result
+      console.log('----', result);
+    }
+  }
+
+  // startReadingSMS().then(() => console.log('listening'));
+
   return (
     <View
       style={{
@@ -9,7 +73,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button onPress={startReadingSMS} title="Request perms"></Button>
+      <Text>{isListening ? 'listening' : 'omg not listening'}</Text>
     </View>
   );
 }
